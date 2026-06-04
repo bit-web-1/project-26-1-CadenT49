bff61bf596dba393d704029d189c84b975211cae
CadenT49 <thomca11@student.op.ac.nz>
2026-06-05 02:17:02 +1200

    changed heading and its styling

--- project2/src/routes/+page.svelte
+++ project2/src/routes/+page.svelte
@@ -4,10 +4,12 @@
   const games = data.games ?? [];
 </script>
 
-<h1>Games</h1>
+<h1>Trophies</h1>
+
+<h2>Games</h2>
 
 {#each games as game}
-  <div style="display:flex; gap:10px; align-items:center; margin-bottom:10px;">
+  <div class="trophy-list">
     
     {#if game.trophyTitleIconUrl}
       <img src={game.trophyTitleIconUrl} width="50" alt={game.trophyTitleName} />
@@ -28,6 +30,25 @@
 <style>
   * {
     font-family: 'Inter', sans-serif;
+  }
 
+  h1 {
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+    text-align: center;
+    background-color: rgb(0, 111, 205);
+    margin: 0;
+    padding: 1rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+  .trophy-list {
+  display:flex; 
+  gap:10px; 
+  align-items:center; 
+  margin-bottom:10px;
   }
 </style>
