a0756d43dbf3d1081f61d4c0e13e91be58d58c27
CadenT49 <thomca11@student.op.ac.nz>
2026-06-16 22:37:56 +1200

    changed layout
    moved all home page content to trophies page to make room for home page

---
+++ project2/src/routes/trophies/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import GameCard from '$lib/components/GameCard.svelte';
+
+    let { data } = $props();
+
+    const games = data.games ?? [];
+</script>
+
+<h2>Games</h2>
+
+<div class="games-grid">
+    {#each games as game}
+        <GameCard {game} />
+    {/each}
+</div>
+
+<style>
+    .games-grid {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 16px;
+    }
+</style>
