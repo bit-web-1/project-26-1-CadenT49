5f08395f7d7a60f14b9cc739ec70dcac36480b39
CadenT49 <thomca11@student.op.ac.nz>
2026-06-18 17:19:41 +1200

    changes to trophy card

--- project2/src/routes/game/[game]/+page.svelte
+++ project2/src/routes/game/[game]/+page.svelte
@@ -1,21 +1,41 @@
 <script>
-	import TrophyDetail from '$lib/components/TrophyDetail.svelte';
+    import TrophyCard from '$lib/components/TrophyDetail.svelte';
 
-    let { data } = $props();
+	let { data } = $props();
 
-    const trophies = data.games ?? [];
+	const game = data.game;
+	const trophies = data.trophies ?? [];
 </script>
 
+{#if game}
+	<h1 class="game-name">{game.trophyTitleName}</h1>
+	<p class="game-platform">{game.trophyTitlePlatform}</p>
 
-<div class="trophies-grid">
-    {#each trophies as trophy}
-    <TrophyDetail {trophy}/>
-    {/each}
-</div>
+	<div class="trophies-grid">
+		{#each trophies as trophy}
+			<TrophyCard {trophy} />
+		{/each}
+	</div>
+{:else}
+	<h1>Game not found</h1>
+{/if}
+
 <style>
-        .trophies-grid {
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        gap: 16px;
+	h1,
+	p {
+		text-align: center;
+	}
+
+	.trophies-grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+		gap: 16px;
+	}
+    .game-name {
+        margin-top: 0.5em;
+    }
+    .game-platform {
+        color: #666;
+        margin-bottom: 1em;
     }
 </style>
